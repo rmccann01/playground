1419f46d66bc062774743cd64ec956c5bd969bbf
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2019-11-11 12:23:49 -0500

    Add Team Agent

--- pommerman/agents/__init__.py
+++ pommerman/agents/__init__.py
@@ -8,3 +8,4 @@ from .random_agent import RandomAgent
 from .simple_agent import SimpleAgent
 from .tensorforce_agent import TensorForceAgent
 from .new_agent import NewAgent
+from .new_agent_team import NewAgentTeam
